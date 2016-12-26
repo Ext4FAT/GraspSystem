f6335beeb94dcb06da5d0829a0653cf963f8b61b
_IDLER_ <zyy34472@gmail.com>
2016-12-27 05:38:31 +0800

    simplify

--- include/VideoDriver.hpp
+++ include/VideoDriver.hpp
@@ -8,16 +8,21 @@ class VideoDriver: public FileOperation
 public:
 	VideoDriver(int width, int height, float fps = 30);
 	int dobotCTRL();
-
-	Mat PXCImage2Mat(PXCImage* pxc);
 	Mat makeChessBoard(int pixels, int count);
+	
+private:
+	void placeWindows(int topk);
+	Mat PXCImage2Mat(PXCImage* pxc);
 	vector<Point2f> findChessBoardCorners(Mat &color, Mat &depth, Size pattern = { 3, 3 });
-
-
-
+	void calArmCoordinate(PXCPoint3DF32 origin, float side);
 private:
 	PXCSizeI32 camera_;
 	pxcF32 fps_;
 	vector<PXCPoint3DF32> vertices_;
+
+	vector<PXCPoint3DF32> corresponding_;
+	PXCPoint3DF32 origin_;
+	float side_;
+
 	//HOG_SVM classification_;
 };
--- src/VideoDriver.cpp
+++ src/VideoDriver.cpp
@@ -24,25 +24,9 @@ void selectPoint(int event, int x, int y, int flags, void* paras)
 	}
 }
 
-//How to build a chessboard
-Mat VideoDriver::makeChessBoard(int pixels, int count)
-//count: black count
-//pixels: for each black or white
-{
-	int len = 2 * pixels * count;
-	Mat chess = Mat::zeros(len, len, CV_8UC3);
-	for (int i = 0; i < chess.rows; i++){
-		for (int j = 0; j < chess.cols; j++){
-			if ((j / pixels + i / pixels) % 2)
-				chess.at<Vec3b>(i, j) = { 255, 255, 255 };
-		}
-	}
-	imwrite("chess" + to_string(count) + ".png", chess);
-	return chess;
-}
 
 //Draw corner points text index
-void drawCornerText(const Mat &color, const Mat &depth, const vector<Point2f> &corners)
+inline void drawCornerText(const Mat &color, const Mat &depth, const vector<Point2f> &corners)
 {
 	int i = 1;
 	for (auto c : corners){
@@ -51,61 +35,35 @@ void drawCornerText(const Mat &color, const Mat &depth, const vector<Point2f> &c
 	}
 }
 
-//Find chessboard from image
-vector<Point2f> VideoDriver::findChessBoardCorners(Mat &color, Mat &depth, Size pattern)
-{
-	vector<Point2f> corners;
-	Mat gray;
-	cv::cvtColor(color, gray, cv::COLOR_BGR2GRAY);
-	bool found = cv::findChessboardCorners(color, pattern, corners);
-	if (corners.size() > 0){
-		cornerSubPix(gray, corners, Size(10, 10), Size(-1, -1), TermCriteria(CV_TERMCRIT_ITER | CV_TERMCRIT_EPS, 500, 0.003));
-		cv::drawChessboardCorners(color, pattern, corners, found);
-		drawCornerText(color, depth, corners);
-		imshow("chess", color);
-	}
-	return corners;
-}
-
 //Calculate arm Coordinate
-vector<PXCPoint3DF32> calArmCoordinate(PXCPoint3DF32 origin, float side)
+void VideoDriver::calArmCoordinate(PXCPoint3DF32 origin, float side)
 {
-	vector<PXCPoint3DF32> corresponding;
 	MESSAGE_COUT("Corresponding Points", "");
 	for (int i = 0; i < 3; i++){
 		for (int j = 0; j < 3; j++){
 			PXCPoint3DF32 tmp = origin;
 			tmp.x += j*side;
 			tmp.y -= i*side;
-			corresponding.push_back(tmp);
+			corresponding_.push_back(tmp);
 			MESSAGE_COUT(i << "," << j, tmp.x << "," << tmp.y << "," << tmp.z);
 		}
 	}
 	int i = 0;
-	for (auto c : corresponding)
+	for (auto c : corresponding_)
 		MESSAGE_COUT(++i, c.x << "," << c.y << "," << c.z);
-	return corresponding;
 }
 
-//Create Thread to Show Image 
-void myimshow(const string winname, Mat &img)
+// Construct
+VideoDriver::VideoDriver(int width, int height, float fps)
 {
-	imshow(winname, img);
-}
-void threadShow(const string winname, Mat &img)
-{
-	thread t(myimshow, winname, img);
-	t.detach();
+	camera_.width = width;
+	camera_.height = height;
+	fps_ = fps;
 }
 
-//Show boundbox and word
-inline void drawText(Mat &img, Rect &boundbox, const string content)
-{
-	putText(img, content, (boundbox.tl() + boundbox.br()) / 2, 3, 0.6, Scalar(0, 0, 255), 2);
-}
 
 // Locate windows position
-void placeWindows(int topk)
+void VideoDriver::placeWindows(int topk)
 {
 	cv::namedWindow("depth");
 	cv::namedWindow("color");
@@ -125,33 +83,6 @@ void placeWindows(int topk)
 	}
 }
 
-// Construct
-VideoDriver::VideoDriver(int width, int height, float fps)
-{
-	camera_.width = width;
-	camera_.height = height;
-}
-
-//Convert RealSense's PXCImage to Opencv's Mat
-Mat VideoDriver::PXCImage2Mat(PXCImage* pxc)
-{
-	if (!pxc)	return Mat(0, 0, 0);
-	PXCImage::ImageInfo info = pxc->QueryInfo();
-	PXCImage::ImageData data;
-	Mat cvt;
-	if (info.format & PXCImage::PIXEL_FORMAT_YUY2) {	//color data
-		if (pxc->AcquireAccess(PXCImage::ACCESS_READ, PXCImage::PIXEL_FORMAT_RGB24, &data) < PXC_STATUS_NO_ERROR)
-			return  Mat(0, 0, 0);
-		cvt = Mat(info.height, info.width, CV_8UC3, (void*)data.planes[0],data.pitches[0]/sizeof(uchar));
-	}
-	else if (info.format & PXCImage::PIXEL_FORMAT_DEPTH) {//depth data
-		if (pxc->AcquireAccess(PXCImage::ACCESS_READ, PXCImage::PIXEL_FORMAT_DEPTH, &data) < PXC_STATUS_NO_ERROR)
-			return  Mat(0, 0, 0);
-		cvt = Mat(info.height, info.width, CV_16U, (void*)data.planes[0], data.pitches[0] / sizeof(uchar));	//Mat初始化是按照长宽来定的
-	}
-	pxc->ReleaseAccess(&data);
-	return cvt;
-}
 
 //Drive Dobot work
 int VideoDriver::dobotCTRL()
@@ -203,7 +134,7 @@ int VideoDriver::dobotCTRL()
 	float side = 71.0f;
 	Size pattern = { 3, 3 };
 	vector<Point2f> corners;
-	vector<PXCPoint3DF32> corresponding = calArmCoordinate(origin, side);
+	calArmCoordinate(origin, side);
 	Mat trans = Mat::eye(4, 4, CV_32FC1);
 	// Calibration Flag
 	bool calibrated = false;
@@ -251,7 +182,7 @@ int VideoDriver::dobotCTRL()
 							tmp.at<float>(0, 2) = v.z;
 							src.push_back(tmp);
 							Mat tmp2 = Mat::ones(1, 4, CV_32FC1);
-							PXCPoint3DF32 cor = corresponding[index];
+							PXCPoint3DF32 cor = corresponding_[index];
 							tmp2.at<float>(0, 0) = cor.x;
 							tmp2.at<float>(0, 1) = cor.y;
 							tmp2.at<float>(0, 2) = cor.z;
@@ -317,3 +248,55 @@ int VideoDriver::dobotCTRL()
 
 }
 
+
+//Convert RealSense's PXCImage to Opencv's Mat
+Mat VideoDriver::PXCImage2Mat(PXCImage* pxc)
+{
+	if (!pxc)	return Mat(0, 0, 0);
+	PXCImage::ImageInfo info = pxc->QueryInfo();
+	PXCImage::ImageData data;
+	Mat cvt;
+	if (info.format & PXCImage::PIXEL_FORMAT_YUY2) {	//color data
+		if (pxc->AcquireAccess(PXCImage::ACCESS_READ, PXCImage::PIXEL_FORMAT_RGB24, &data) < PXC_STATUS_NO_ERROR)
+			return  Mat(0, 0, 0);
+		cvt = Mat(info.height, info.width, CV_8UC3, (void*)data.planes[0], data.pitches[0] / sizeof(uchar));
+	}
+	else if (info.format & PXCImage::PIXEL_FORMAT_DEPTH) {//depth data
+		if (pxc->AcquireAccess(PXCImage::ACCESS_READ, PXCImage::PIXEL_FORMAT_DEPTH, &data) < PXC_STATUS_NO_ERROR)
+			return  Mat(0, 0, 0);
+		cvt = Mat(info.height, info.width, CV_16U, (void*)data.planes[0], data.pitches[0] / sizeof(uchar));	//Mat初始化是按照长宽来定的
+	}
+	pxc->ReleaseAccess(&data);
+	return cvt;
+}
+
+//Find chessboard from image
+vector<Point2f> VideoDriver::findChessBoardCorners(Mat &color, Mat &depth, Size pattern)
+{
+	vector<Point2f> corners;
+	Mat gray;
+	cv::cvtColor(color, gray, cv::COLOR_BGR2GRAY);
+	bool found = cv::findChessboardCorners(color, pattern, corners);
+	if (corners.size() > 0){
+		cornerSubPix(gray, corners, Size(10, 10), Size(-1, -1), TermCriteria(CV_TERMCRIT_ITER | CV_TERMCRIT_EPS, 500, 0.003));
+		cv::drawChessboardCorners(color, pattern, corners, found);
+		drawCornerText(color, depth, corners);
+		imshow("chess", color);
+	}
+	return corners;
+}
+
+//How to build a chessboard
+Mat VideoDriver::makeChessBoard(int pixels, int count)
+{
+	int len = 2 * pixels * count;
+	Mat chess = Mat::zeros(len, len, CV_8UC3);
+	for (int i = 0; i < chess.rows; i++){
+		for (int j = 0; j < chess.cols; j++){
+			if ((j / pixels + i / pixels) % 2)
+				chess.at<Vec3b>(i, j) = { 255, 255, 255 };
+		}
+	}
+	imwrite("chess" + to_string(count) + ".png", chess);
+	return chess;
+}
