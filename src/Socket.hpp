fd6a17034da54671482d5f18e7368276b88170cc
_IDLER_ <zyy34472@gmail.com>
2017-03-09 21:16:07 +0800

    minor

--- src/Socket.hpp
+++ src/Socket.hpp
@@ -3,20 +3,16 @@
 
 int MySend(string buf)
 {
-	const int BUF_SIZE = 64;
-	WSADATA         wsd;            //WSADATA变量  
-	SOCKET          sHost;          //服务器套接字  
-	SOCKADDR_IN     servAddr;       //服务器地址  
-	int             retVal;         //返回值  
+	WSADATA         wsd;            
+	SOCKET          sHost;          
+	SOCKADDR_IN     servAddr;       
+	int             retVal;       
 
-	//初始化套结字动态库  
-	if (WSAStartup(MAKEWORD(2, 2), &wsd) != 0)
-	{
+	if (WSAStartup(MAKEWORD(2, 2), &wsd) != 0) 	{
 		cout << "WSAStartup failed!" << endl;
 		return -1;
 	}
 
-	//创建套接字  
 	sHost = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 	if (INVALID_SOCKET == sHost)
 	{
@@ -24,33 +20,30 @@ int MySend(string buf)
 		WSACleanup();//释放套接字资源  
 		return  -1;
 	}
-	//设置服务器地址  
+
 	servAddr.sin_family = AF_INET;
 	servAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
 	servAddr.sin_port = htons(11229);
-	int nServAddlen = sizeof(servAddr);
-	//连接服务器  
+ 
 	retVal = connect(sHost, (LPSOCKADDR)&servAddr, sizeof(servAddr));
-	if (SOCKET_ERROR == retVal)
-	{
+	if (SOCKET_ERROR == retVal)	{
 		cout << "connect failed!" << endl;
-		closesocket(sHost); //关闭套接字  
-		WSACleanup();       //释放套接字资源  
+		closesocket(sHost); 
+		WSACleanup();       
 		return -1;
 	}
 
 	retVal = send(sHost, buf.c_str(), buf.size(), 0);
-	if (SOCKET_ERROR == retVal)
-	{
+	if (SOCKET_ERROR == retVal)	{
 		cout << "send failed!" << endl;
-		closesocket(sHost); //关闭套接字  
-		WSACleanup();       //释放套接字资源  
+		closesocket(sHost); 
+		WSACleanup();       
 		return -1;
 	}
 	retVal = send(sHost, "", 0, 0);
 
 	//退出  
-	closesocket(sHost); //关闭套接字  
-	WSACleanup();       //释放套接字资源  
+	closesocket(sHost); 
+	WSACleanup();       
 	return 0;
 }
